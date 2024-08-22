386876e4a4e4012f79da169832c594921db3e621
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-08-22 15:03:56 +0900

    feat: BuildContext with Theme

--- lib/main2.dart
+++ lib/main2.dart
@@ -12,55 +12,46 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
-  int counter = 0;
-  List<int> numbers = [];
-  
-  void onClicked() {
-    // State가 변경되는 점이 있다고 알려주는 함수(refresh?)
-    // 무조건 넣어야 하는건 아니다. counter += 1;이 바깥에 있어도 된다.
-    // setState(){}가 호출되면 일단 UI를 다시 그린다고 생각하면 된다.
-    setState(() {
-      counter += 1;
-      numbers.add(numbers.length);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
+      // 앱 테마 설정
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          )
+        )
+      ),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Counter',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              for (var i in numbers)
-                Text(
-                  '$i',
-                  style: const TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-              IconButton(
-                onPressed: onClicked,
-                icon: const Icon(Icons.add_box_rounded),
-                iconSize: 40,
-              )
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        color: Theme.of(context).textTheme.titleLarge!.color,
+        // color: Theme.of(context).textTheme.titleLarge?.color,
+        fontSize: 30,
       ),
     );
   }

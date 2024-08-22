5ed5fff9418355ba9eb715026530f837f0dc289c
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-08-22 15:18:09 +0900

    feat: Widget life cycle with dispose

--- lib/main2.dart
+++ lib/main2.dart
@@ -12,6 +12,15 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
+  bool showTitle = true; 
+  // Title 위젯을 dispose하는 변수 dispose를 통해 위젯 트리에서 삭제하고
+  // 라이프 사이클을 지킬 수 있다?.
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,13 +33,14 @@ class _CounterState extends State<Counter> {
           )
         )
       ),
-      home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? const MyLargeTitle() : const Text('Nothing!'),
+              IconButton(onPressed: toggleTitle, icon: const Icon(Icons.remove_red_eye))
             ],
           ),
         ),
@@ -39,13 +49,37 @@ class _CounterState extends State<Counter> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
+  
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+
+  // state를 초기화 할 때 사용, 단 한번만 실행됨(viewModel의 init과 동일한듯?)
+  // build 전에 실행이 됨
+  @override
+  void initState() {
+    super.initState();
+    print('init!');
+  }
+
+  // 위젯이 위젯 트리에서 제거될 때
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose!');
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('build!');
     return Text(
       'My Large Title',
       style: TextStyle(
